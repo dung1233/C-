42edaee25f6ec5ee6c8d2c32638f49f1ac43d31c
dung1233 <139690716+dung1233@users.noreply.github.com>
2024-03-14 10:53:59 +0700

    Update Program.cs

--- Exam02/Exam02/Program.cs
+++ Exam02/Exam02/Program.cs
@@ -1,44 +1,20 @@
-﻿using System;
+using System;
 
-namespace InheritancePolymorphismExample
+class Program
 {
-    class Animal
+    static void Main(string[] args)
     {
-        protected string Name { get; set; }
-        protected double Weight { get; set; }
+      
+        Lion lion = new Lion(100, "Lion");
+        Tiger tiger = new Tiger(200, "Tiger");
 
-        public Animal(double weight, string name)
+      
+        Animal[] animals = new Animal[] { lion, tiger };
+
+       
+        foreach (var animal in animals)
         {
-            Weight = weight;
-            Name = name;
-        }
-
-        public virtual void Show()
-        {
-            Console.WriteLine($"Name: {Name}, Weight: {Weight}kg");
-        }
-    }
-
-    class Lion : Animal
-    {
-        public Lion(double weight, string name) : base(weight, name) { }
-    }
-
-    class Tiger : Animal
-    {
-        public Tiger(double weight, string name) : base(weight, name) { }
-    }
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Lion lion = new Lion(100, "Tiger");
-            Tiger tiger = new Tiger(180, "lion");
-
-            
-            lion.Show();
-            tiger.Show();
+            animal.Show();
         }
     }
 }
